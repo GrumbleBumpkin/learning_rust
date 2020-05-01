f9b8e40ad1c422bfdfa39dfe6865250167fa531b
Bryndon Lee <bryndontlee.git@gmail.com>
2020-05-01 13:35:33 -0500

    Shadowing exercise

--- variables/src/main.rs
+++ variables/src/main.rs
@@ -1,6 +1,14 @@
 fn main() {
-    let mut x = 5;
-    println!("the value of x is: {}", x);
-    x = 6;
-    println!("the value of x is: {}", x);
+    let x = 5;
+    let x = x + 1;
+    let x = x * 2;
+    println!("The value of x is: {}", x);
+
+    let spaces = "      ";
+    let spaces = spaces.len();
+    println!("The number of spaces is: {}", spaces);
+
+    // Can't change mutable type
+    // let mut mut_spaces = "      ";
+    // mut_spaces = mut_spaces.len();
 }
