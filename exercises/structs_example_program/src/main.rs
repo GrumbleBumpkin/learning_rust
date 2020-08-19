8a0daf2e515f869db22ba9ff47e75e829e871a43
Bryndon Lee <bryndontlee.git@gmail.com>
2020-08-19 12:07:37 -0500

    Example program with associated functions and other tests

--- exercises/structs_example_program/src/main.rs
+++ exercises/structs_example_program/src/main.rs
@@ -1,21 +1,59 @@
 #[derive(Debug)]
 struct Rectangle {
-    width: u32,
-    height: u32,
+    width: f32,
+    height: f32,
 }
 
 impl Rectangle {
-    fn area(&self) -> u32 {
+    fn area(&self) -> f32 {
         self.width * self.height
+    }
+
+    fn can_hold(&self, other_rect: &Rectangle) -> bool {
+        self.height > other_rect.height && self.width > other_rect.width
+    }
+}
+
+impl Rectangle {
+    fn square(length: f32) -> Rectangle {
+        Rectangle {
+            width: length,
+            height: length,
+        }
+    }
+    
+    fn golden_ratio(length: f32) -> Rectangle {
+        Rectangle {
+            width: length * 1.61,
+            height: length,
+        }
     }
 }
 
 fn main() {
     let rect1 = Rectangle {
-        width: 30,
-        height: 50,
+        width: 30 as f32,
+        height: 50 as f32,
     };
-
     println!("rect1 is {:#?}", rect1);
-    println!("The area of the rectangle is {} square pixels", rect1.area());
+    println!("The area of the rectangle is {} square inches", rect1.area());
+
+    let rect2 = Rectangle {
+        width: 10 as f32,
+        height: 40 as f32,
+    };
+    println!("rect1 can hold rect2: {}", rect1.can_hold(&rect2));
+
+    let rect3 = Rectangle {
+        width: 60 as f32,
+        height: 45 as f32,
+    };
+    println!("rect1 can hold rect2: {}", rect1.can_hold(&rect3));
+
+    let golden_rect = Rectangle::golden_ratio(10 as f32);
+
+    println!("The width of golden_rect is {}", golden_rect.width);
+
+    let square = Rectangle::square(5 as f32);
+    assert_eq!(square.width, square.height);
 }
